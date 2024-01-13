79b8d7e6ee71865d2ce891a41f8d09e1509d5913
ilona <gogiashvili.ilona@gmail.com>
2024-01-13 07:57:15 -0500

    updated

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 const About = () => {
-    return (
-        <div className='home'>
-            <div className='about'>
-                Our team has more than 10 years of experience in the tourism business!
-                <rb />
-                Today, the system is able to solve most of the routine issues: a database of partner tour operators in one place, automatic generation of documents, control of applications, on-line availability of places on tours, formation of transport location schemes, automatic creation of routes for groups and much more.
-                <rb />
-                The system is constantly being improved, the necessary tools are being introduced, which significantly save time and resources of users.
-            </div>
-            <img width="100%" src="https://www.letsroam.com/explorer/wp-content/uploads/sites/10/2022/02/best-family-vacations-on-a-budget.jpg" alt="beach" />
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <div className="about">
+        <rb />
+        Today, the system is able to solve most of the routine issues: a
+        database of partner tour operators in one place, automatic generation of
+        documents, control of applications, on-line availability of places on
+        tours, formation of transport location schemes, automatic creation of
+        routes for groups and much more.
+        <rb />
+        The system is constantly being improved, the necessary tools are being
+        introduced, which significantly save time and resources of users.
+      </div>
+      <img
+        width="100%"
+        src="https://www.letsroam.com/explorer/wp-content/uploads/sites/10/2022/02/best-family-vacations-on-a-budget.jpg"
+        alt="beach"
+      />
+    </div>
+  );
+};
 
-export default About
-
+export default About;
